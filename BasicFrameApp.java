b9794960665407f1dd612a13c4d0b5229bbd7b85
MNekhubvi <139873774+MNekhubvi@users.noreply.github.com>
2023-07-18 13:59:23 +0200

    Add files via upload

---
+++ BasicFrameApp.java
@@ -0,0 +1,18 @@
+
+package basicframeapp;
+
+import za.ac.tut.ui.MyFirstFrame;
+
+
+public class BasicFrameApp {
+
+   
+    public static void main(String[] args)
+    {
+         MyFirstFrame myFrame;
+         
+         myFrame = new MyFirstFrame();
+         
+    }
+    
+}
